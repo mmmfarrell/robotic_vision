a779f936623fbad749af4384208efb00c8b240c0
michael <michaeldavidfarrell@gmail.com>
2018-03-02 16:44:22 -0700

    progress on visual odom vs truth

--- visual_odom/visual_odom.cpp
+++ visual_odom/visual_odom.cpp
@@ -8,7 +8,7 @@ namespace robotic_vision
 
 VisualOdom::VisualOdom()
 {
-  cout << "Constructor" << endl;
+  //cout << "Constructor" << endl;
 
   // Set thresholds
   ransac_thresh_ = 2.5f;
@@ -72,7 +72,7 @@ Mat VisualOdom::calcOdom(Mat img1, Mat img2)
     vector<Point2f> mpoints1, mpoints2;
     kp1[1].convert(matched1, mpoints1);
     kp2[1].convert(matched2, mpoints2);
-    cout << "Mpoints length: " << mpoints1.size() << endl;
+    //cout << "Mpoints length: " << mpoints2.size() << endl;
 
     // Compute essential matrix then recover pose
     //homography = findHomography(mpoints1, mpoints2, RANSAC, ransac_thresh_, inlier_mask);
@@ -95,6 +95,7 @@ Mat VisualOdom::calcOdom(Mat img1, Mat img2)
   if (matched1.size() < 4 || E.empty() )
   {
     hconcat(img1, img2, res);
+    cout << "No update" << endl;
   }
   else
   {
@@ -120,11 +121,96 @@ Mat VisualOdom::calcOdom(Mat img1, Mat img2)
 
 } // end namespace
 
+// From Learn OpenCV
+// Checks if a matrix is a valid rotation matrix.
+bool isRotationMatrix(Mat &R)
+{
+    Mat Rt;
+    transpose(R, Rt);
+    Mat shouldBeIdentity = Rt * R;
+    Mat I = Mat::eye(3,3, shouldBeIdentity.type());
+     
+    return  norm(I, shouldBeIdentity) < 1e-6;
+     
+}
+ 
+// Calculates rotation matrix to euler angles
+// The result is the same as MATLAB except the order
+// of the euler angles ( x and z are swapped ).
+Vec3f rotationMatrixToEulerAngles(Mat &R)
+{
+ 
+    assert(isRotationMatrix(R));
+     
+    float sy = sqrt(R.at<double>(0,0) * R.at<double>(0,0) +  R.at<double>(1,0) * R.at<double>(1,0) );
+ 
+    bool singular = sy < 1e-6; // If
+ 
+    float x, y, z;
+    if (!singular)
+    {
+        x = atan2(R.at<double>(2,1) , R.at<double>(2,2));
+        y = atan2(-R.at<double>(2,0), sy);
+        z = atan2(R.at<double>(1,0), R.at<double>(0,0));
+    }
+    else
+    {
+        x = atan2(-R.at<double>(1,2), R.at<double>(1,1));
+        y = atan2(-R.at<double>(2,0), sy);
+        z = 0;
+    }
+    return Vec3f(x, y, z);
+     
+     
+     
+}
+
+Mat plotTruth()
+{
+  Mat traj = Mat::zeros(600, 600, CV_8UC3);
+
+  ifstream myfile("/home/michael/gradschool/Winter18/robotic_vision/datasets/dataset/sequences/poses/00.txt");
+
+  double x, y, z, var;
+  //String line;
+  char line[256];
+
+  if (myfile.is_open())
+  {
+    while (!myfile.eof())
+    {
+      myfile.getline(line, 256);
+      std::istringstream in(line);
+
+      for (int i = 0; i < 12; i++)
+      {
+        in >> var;
+        if (i == 3)
+          x = var;
+        if (i == 7)
+          y = var;
+        if (i == 11)
+          z = var;
+        //cout << "x: " << x << endl;
+      }
+      circle(traj, Point(x + 300, z + 100), 1, Scalar(0, 0, 255), 2);
+      imshow("Truth", traj);
+      char c(0);
+      c = (char)waitKey(1);
+      if ( c == 27 )
+        break;
+    }
+  }
+  //cout << "end" << endl;
+  myfile.close();
+  return traj;
+}
+
 int main(int argc, char** argv)
 {
 
   // TODO: Remove
-  int num_frames = 1000;
+  int num_frames = 4500;
 
   // Instantiate VisualOdom class
   robotic_vision::VisualOdom vo;
@@ -133,9 +219,15 @@ int main(int argc, char** argv)
   Mat img1, img2, res;
   char filename1[200], filename2[200];
 
+  // Mat for trajectory
+  Mat traj = Mat::zeros(600, 600, CV_8UC3);
+  traj = plotTruth();
 
   for (int i = 0; i < num_frames; i++)
   {
+    clock_t t;
+    t = clock();
+
     sprintf(filename1, "/home/michael/gradschool/Winter18/robotic_vision/datasets/dataset/sequences/00/image_0/%06d.png", i);
     sprintf(filename2, "/home/michael/gradschool/Winter18/robotic_vision/datasets/dataset/sequences/00/image_0/%06d.png", i+1);
 
@@ -143,13 +235,25 @@ int main(int argc, char** argv)
     img2 = imread(filename2, CV_LOAD_IMAGE_GRAYSCALE);
 
     // Get VO
-    //clock_t t;
-    //t = clock();
     res = vo.calcOdom(img1, img2);
-    //t = clock() - t;
-    //std::printf("I can run at @ %f HZ.\n", (CLOCKS_PER_SEC/(float)t));
+
+    // Get current euler angles
+    Vec3f euler;
+    euler = rotationMatrixToEulerAngles(vo.R_);
+    //cout << "Euler angles: " << euler << endl;
+    //cout << "Translation: " << vo.t_ << endl;
+
+    // Plot trajectory
+    int x_traj = int(vo.t_.at<double>(0)) + 100;
+    int y_traj = int(vo.t_.at<double>(2)) + 400;
+    circle(traj, Point(x_traj, y_traj), 1, Scalar(255, 0, 0), 2);
+
+    // Determine runtime
+    t = clock() - t;
+    std::printf("I can run at @ %f HZ.\n", (CLOCKS_PER_SEC/(float)t));
 
     imshow("Matches", res);
+    imshow("Trajectory", traj);
     char c(0);
     c = (char)waitKey(2);
     if ( c == 27 )
